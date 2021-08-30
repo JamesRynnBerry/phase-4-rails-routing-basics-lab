5768122a55452a14eebe663256c0e67b82cbd191
James Berry <84353254+JamesRynnBerry@users.noreply.github.com>
2021-08-30 12:36:11 -0400

    Completed Assignment

--- spec/requests/grades_spec.rb
+++ spec/requests/grades_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Students", type: :request do
+RSpec.describe "Grades", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
